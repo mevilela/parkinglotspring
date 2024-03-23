9caf698dc7cf6f5a010905d4c4d38ca073cdd587
sallo.szrajbman <salloszrajbman@hotmail.com>
2024-03-23 14:45:07 +0000

    Refactoring create parking ticket / lot

--- src/main/java/zely/parkinglotspring/manager/PaymentManager.java
+++ src/main/java/zely/parkinglotspring/manager/PaymentManager.java
@@ -2,11 +2,11 @@ package zely.parkinglotspring.manager;
 
 import org.springframework.stereotype.Service;
 import zely.parkinglotspring.dto.PaymentDto;
-import zely.parkinglotspring.factory.PaymentServiceFactory;
 import zely.parkinglotspring.model.parkingticket.ParkingTicket;
 import zely.parkinglotspring.model.payment.Cash;
 import zely.parkinglotspring.model.payment.CreditCard;
 import zely.parkinglotspring.model.payment.Payment;
+import zely.parkinglotspring.model.payment.PaymentStatus;
 import zely.parkinglotspring.service.parkingticket.ParkingTicketService;
 import zely.parkinglotspring.service.payment.PaymentService;
 
@@ -14,10 +14,13 @@ import java.util.Optional;
 
 @Service
 public class PaymentManager {
+
+    PaymentService paymentService;
     ParkingTicketService parkingTicketService;
 
-    public PaymentManager(ParkingTicketService parkingTicketService) {
+    public PaymentManager(ParkingTicketService parkingTicketService, PaymentService paymentService) {
         this.parkingTicketService = parkingTicketService;
+        this.paymentService = paymentService;
     }
 
     public Payment processPayment(PaymentDto paymentDto) {
@@ -30,23 +33,29 @@ public class PaymentManager {
 
         ParkingTicket ticket = ticketOptional.get();
 
-        Payment newPayment = switch (paymentDto.getPaymentMethod()){
-            case "credicard" -> new CreditCard();
-            case "cash" -> new Cash();
-            default -> throw new RuntimeException("Unknown payment method informed");
-        };
+        if(ticket.getPayment() != null && ticket.getPayment().getPaymentStatus().equals(PaymentStatus.COMPLETED))  {
+            throw new RuntimeException("Ticket already paid.");
+        }
 
-        newPayment.setParkingTicket(ticket);
+        // if payment exists, just load it from the ticket
+        // otherwise create new payment
+        Payment payment = ticket.getPayment() != null ? ticket.getPayment() :
+                switch (paymentDto.getPaymentMethod()){
+                    case "credicard" -> new CreditCard();
+                    case "cash" -> new Cash();
+                    default -> throw new RuntimeException("Unknown payment method informed");
+                };
 
-        PaymentService paymentService = PaymentServiceFactory.getPaymentService(paymentDto.getPaymentMethod());
+        payment.setAmount(paymentDto.getAmountToPay());
+        payment.setParkingTicket(ticket);
 
-        newPayment = paymentService.processPayment(newPayment);
+        payment = paymentService.processPayment(payment);
 
-        ticket.setPayment(newPayment);
+        ticket.setPayment(payment);
 
         parkingTicketService.updateParkingTicket(ticket);
 
-        return newPayment;
+        return payment;
 
     }
 
--- src/main/java/zely/parkinglotspring/service/payment/PaymentService.java
+++ src/main/java/zely/parkinglotspring/service/payment/PaymentService.java
@@ -1,8 +1,29 @@
 package zely.parkinglotspring.service.payment;
 
+import org.springframework.stereotype.Service;
+import zely.parkinglotspring.model.payment.Cash;
+import zely.parkinglotspring.model.payment.CreditCard;
 import zely.parkinglotspring.model.payment.Payment;
+import zely.parkinglotspring.model.payment.PaymentStatus;
+import zely.parkinglotspring.repository.payment.PaymentRepository;
 
-public interface PaymentService {
+@Service
+public class PaymentService {
 
-    Payment processPayment(Payment payment);
+    private final PaymentRepository paymentRepository;
+
+    public PaymentService(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public Payment processPayment(Payment payment) {
+
+        if (payment.getAmount() != payment.getParkingTicket().getAmount()){
+            payment.setPaymentStatus(PaymentStatus.FAILED);
+        } else {
+            payment.setPaymentStatus(PaymentStatus.COMPLETED);
+        }
+
+        return paymentRepository.save(payment);
+    }
 }
